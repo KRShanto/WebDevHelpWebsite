cbc91232c765f6c7d3adcacefae2432aacb8e3dc
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2022-12-19 20:34:36 +0600

    Improve styling of the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,34 +3,9 @@ import React from "react";
 export default function index() {
   return (
     <>
-      <div
-        className="intro"
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          fontSize: "1.5rem",
-        }}
-      >
-        <h2
-          className="first-intro"
-          style={{
-            // light cyan
-            color: "#73fffa",
-            // textShadow: "0 0 2px #73fffa",
-          }}
-        >
-          Web Developer Help
-        </h2>
-        <h1
-          className="second-intro"
-          style={{
-            color: "cyan",
-            textShadow: "0 0 100px cyan",
-          }}
-        >
+      <div className="intro">
+        <h2 className="first-intro">Web Developer Help</h2>
+        <h1 className="second-intro">
           We are a community to help the community
         </h1>
       </div>
