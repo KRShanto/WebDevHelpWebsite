5711d3c7fdaeaea8a5baf34ee5e7983a17b7546f
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2023-01-01 13:14:50 +0600

    Create a navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="logo">WebDevHelp</div>
+      <div className="links">
+        <Link href="/" className="link">
+          Home
+        </Link>
+
+        <Link href="/chat" className="link">
+          Chat
+        </Link>
+
+        <Link href="/signup" className="link">
+          Signup
+        </Link>
+
+        <a
+          href="https://github.com/WebDevHelpBD/backend"
+          className="link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </div>
+    </nav>
+  );
+}
