c3ef55cb42d9cbfc9a9b7eb9f9f80c8865a7b839
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2023-01-01 13:15:31 +0600

    Add a loading bar when page changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,43 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import Navbar from "../components/Navbar";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import LoadingBar from "react-top-loading-bar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [progress, setProgress] = useState(0);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      setProgress(30);
+    };
+    const handleComplete = (url: string) => {
+      setProgress(100);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+
+  return (
+    <>
+      <Navbar />
+      <LoadingBar
+        color="#ff0084"
+        height={3}
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
