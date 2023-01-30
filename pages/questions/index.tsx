9f57569ae88b8130e732e51b885a0e4840475b57
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2023-01-30 16:08:12 +0600

    Create questions page where questions will be appear

--- pages/questions/index.tsx
+++ pages/questions/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Link from "next/link";
+import dbConnect from "../../lib/dbConnect";
+import Question from "../../models/Question";
+import Answer from "../../models/Answer";
 
-export default function QuestionsPage() {
+export default function QuestionsPage({ questions }: any) {
   return (
     <div className="questions-div">
       <div className="header">
@@ -18,7 +21,53 @@ export default function QuestionsPage() {
         <Link href="/questions/ask" className="ask-question">
           Ask Question
         </Link>
+
+        <div className="display-questions">
+          {questions.map((question: any) => (
+            <div className="question" key={question._id}>
+              <div className="user">
+                <p className="name">
+                  {question.user.name}
+                  {/* TODO: image */}
+                </p>
+              </div>
+
+              <div className="title">
+                <Link href={`/questions/${question.shortId}/${question.title}`}>
+                  {question.title}
+                </Link>
+              </div>
+              <div className="tags">
+                {question.tags.map((tag: string, index: number) => (
+                  <span className="tag" key={index}>
+                    {tag}
+                  </span>
+                ))}
+              </div>
+              <div className="vote-count">{question.upVotes} Upvotes</div>
+              <div className="answer-count">{question.answers} Answers</div>
+
+              <div className="date">
+                {new Date(question.createdAt).toLocaleDateString()}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context: any) {
+  await dbConnect();
+
+  const questions = await Question.find({}).sort({ createdAt: -1 });
+
+  // console.log(questions);
+
+  return {
+    props: {
+      questions: JSON.parse(JSON.stringify(questions)),
+    },
+  };
 }
